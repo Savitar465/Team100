9f8530c0000f8ac80e1906f7dcaef90838c51184
Savitar465 <55471147+Savitar465@users.noreply.github.com>
2020-12-06 20:45:01 -0400

    Initial commit

---
+++ src/main/java/com/tema100/demo/Team100Application.java
@@ -0,0 +1,13 @@
+package com.tema100.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team100Application.class, args);
+	}
+	
+}
---
+++ src/test/java/com/tema100/demo/Team100ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tema100.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team100ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
